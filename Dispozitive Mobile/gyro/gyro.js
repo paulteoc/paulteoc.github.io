ccb9f5d1ac9ab9bb9ad34fddc50a53fe3ff7e51d
paulteoc <56435952+paulteoc@users.noreply.github.com>
2019-11-04 10:47:54 +0200

    Add files via upload

--- Dispozitive Mobile/gyro/gyro.js
+++ Dispozitive Mobile/gyro/gyro.js
@@ -1,10 +1,10 @@
-document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.0";
+document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.1";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 
 function on_gyro_data_uab(e)
 {
-    document.getElementById("id_alpha").innerHTML = e.alpha;
-    document.getElementById("id_beta").innerHTML = e.beta;
-    document.getElementById("id_gamma").innerHTML = e.gamma;
+    document.getElementById("id_alpha").innerHTML = Math.round(e.alpha * 100) / 100;
+    document.getElementById("id_beta").innerHTML = Math.round(e.beta * 100) / 100;
+    document.getElementById("id_gamma").innerHTML = Math.round(e.gamma * 100) / 100;
 }
