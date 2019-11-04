8561f515e6c4ab49450500aaf46295130d883541
paulteoc <56435952+paulteoc@users.noreply.github.com>
2019-11-04 12:08:16 +0200

    Add files via upload

--- Dispozitive Mobile/gyro/gyro.js
+++ Dispozitive Mobile/gyro/gyro.js
@@ -1,7 +1,8 @@
-document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.2";
+document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.3";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
+
 
 function on_gyro_data_uab(e)
 {
@@ -15,4 +16,10 @@ function on_acc_data_uab(e)
     document.getElementById("id_acc_x").innerHTML = Math.round(acc.x * 100) / 100;
     document.getElementById("id_acc_y").innerHTML = Math.round(acc.y * 100) / 100;
     document.getElementById("id_acc_z").innerHTML = Math.round(acc.z * 100) / 100;
+
+    var rot_x = Math.atan(acc.x / acc.z) * 180 / Math.PI;
+    var rot_y = Math.atan(acc.y / acc.z) * 180 / Math.PI;
+    
+    document.getElementById("id_rot_x").innerHTML = Math.round(rot_x * 100) / 100;
+    document.getElementById("id_rot_y").innerHTML = Math.round(rot_y * 100) / 100;
 }
