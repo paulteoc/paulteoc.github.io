3ad708c7a2a08c455f5e7026ec5c7e2f482d3200
paulteoc <56435952+paulteoc@users.noreply.github.com>
2019-11-22 12:34:44 +0200

    Add files via upload

---
+++ HTML/wk/calcul_prime.js
@@ -0,0 +1,12 @@
+function este_prim(n)
+{
+	for (var i = 2; i * i <= n; i++)
+		if (n % i == 0)
+			return true;
+	return false;	
+}
+
+for (var i = 1E11; i < 1E12; i++)
+	if (este_prim(i)){
+		postMessage(i);
+	}
