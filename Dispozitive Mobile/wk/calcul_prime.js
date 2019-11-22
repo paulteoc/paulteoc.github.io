e45a6cb7a7e3ef7279ff1a00f2dbf09271ef43b4
paulteoc <56435952+paulteoc@users.noreply.github.com>
2019-11-22 13:20:56 +0200

    Add files via upload

--- Dispozitive Mobile/wk/calcul_prime.js
+++ Dispozitive Mobile/wk/calcul_prime.js
@@ -1,12 +1,36 @@
-function este_prim(n)
-{
-	for (var i = 2; i * i <= n; i++)
-		if (n % i == 0)
-			return true;
-	return false;	
+var ruleaza = false;
+
+onmessage = function (e) {
+    if (e.data == "start") {
+        ruleaza = true;
+    } else
+    if (e.data == "stop") {
+        ruleaza = false;
+    }
 }
 
-for (var i = 1E11; i < 1E12; i++)
-	if (este_prim(i)){
-		postMessage(i);
-	}
+
+function este_prim(n) {
+    for (var i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return true;
+    return false;
+}
+
+function start_calcul(start_numar) {
+    if (ruleaza == false)
+        ;
+    else {
+        for (var i = start_numar; i < start_numar + 100; i++)
+            if (este_prim(i))
+                postMessage(i);
+             start_numar += 100;//cresc pe start numar cu cate 100
+             if(start_numar < 1E12){
+                 setTimeout(start_calcul, 1, start_numar);
+             }
+             else{
+                 postMessage("gata");
+             }
+    }
+}
+start_calcul(1E11); //1E11 = 1 la puterea a 11 a
