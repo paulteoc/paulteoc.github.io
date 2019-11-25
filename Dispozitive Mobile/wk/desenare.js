4716500175e9d6f3a7bdeba6fedff30d812aea45
paulteoc <56435952+paulteoc@users.noreply.github.com>
2019-11-25 13:16:54 +0200

    Add files via upload

--- Dispozitive Mobile/wk/desenare.js
+++ Dispozitive Mobile/wk/desenare.js
@@ -1,54 +1,57 @@
-document.getElementById("id_logic").innerHTML = "Logic version = 2019.11.22.1";
+document.getElementById("id_logic").innerHTML = "2019.11.25.6";
 
-document.getElementById("id_start").addEventListener("click", start);
+document.getElementById("id_start").addEventListener("click", start); //la click ii atasam functia start
 document.getElementById("id_stop").addEventListener("click", stop);
 
 var timer_id;
-var unghi = {};
+var unghi = {}
 unghi.valoare = 0;
-var muncitor = null; //nu declaram variabile fara var in fata(nici odata nu declaram variabile globale decat la inceputul programului)
+var muncitor = null; //testam daca este ceva, daca este null atunci facem unul nou, daca nu este null continuam primul fir de memorie
 
-function desenare(context, canvas, raza_mica, raza_mare, unghi) {
+function desenare(unghi) {
+
+
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+
+    var R = 100; //raza mare pt cercul mare
+    var r = 10; //raza mica pe cerc mic
+
     context.clearRect(0, 0, canvas.width, canvas.height);
-    var x = canvas.width / 2 + raza_mare * Math.cos(unghi.valoare * Math.PI / 180);
-    var y = canvas.height / 2 + raza_mare * Math.sin(unghi.valoare * Math.PI / 180);
-
     context.beginPath();
-    context.arc(x, y, raza_mica, 0, 2 * Math.PI);
+    var x = canvas.width / 2 + R * Math.cos(unghi.valoare / 180 * Math.PI);
+    var y = canvas.height / 2 + R * Math.sin(unghi.valoare / 180 * Math.PI)
+
+    context.arc(x, y, r, 0, 2 * Math.PI);
     context.stroke();
 
     unghi.valoare++;
     if (unghi.valoare == 360)
-        unghi.valoare = 0;
+        unghi.valoare = 1;
+
 }
+
 
 function start() {
     document.getElementById("id_start").disabled = true;
     document.getElementById("id_stop").disabled = false;
-    var canvas = document.getElementById("id_canvas");
-    var context = canvas.getContext("2d");
 
+    timer_id = setInterval(desenare, 20, unghi);
     if (muncitor == null) {
-        muncitor = new Worker("calcul_prime.js");
-        muncitor.onmessage = function (e) {
-            document.getElementById("id_prime").innerHTML = e.data;
+        muncitor = new Worker("prime.js");                          //creiez legatura cu muncitorul(adica prime.js)
+        muncitor.onmessage = function (e)                           //functia onmassage primeste mesaje cu rezultatele creiate de muncitor
+        {
+            document.getElementById("id_prime").innerHTML = e.data;     //aici vor fi afisate mesajele primite
         }
-        muncitor.postMassage("start");
     }
     else
-        muncitor.postMassage("start");
-
-    var raza_mare = 100;
-    var raza_mica = 10;
-
-    timer_id = setInterval(desenare, 20, context, canvas, raza_mica, raza_mare, unghi);
+        muncitor.postMessage("start");                  //altfel in momentul in care apasam start se continua firul de executie
 }
 
 function stop() {
-    clearInterval(timer_id);
-
     document.getElementById("id_start").disabled = false;
     document.getElementById("id_stop").disabled = true;
 
-    muncitor.postMassage("stop");
+    clearInterval(timer_id);
+    muncitor.postMessage("stop");
 }
---
+++ Dispozitive Mobile/wk/prime.js
@@ -0,0 +1,10 @@
+function este_prim(n) {
+    for (var i = 2; i * i <= n; i++)//incercam sa evitam radicalul deoarece este o operatiie costisitoare
+        if (n % i == 0)//calculeaza numerele prime
+            return false;
+    return true
+}
+
+for (var i = 1E10; i < 1E11; i++)
+    if (este_prim(i))
+        postMessage(i); //cu postmassage trimit nr i
