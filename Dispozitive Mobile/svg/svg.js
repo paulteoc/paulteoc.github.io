3a3a3fdd3635fb9c68b81dafbd5c29896bfbfe1e
paulteoc <56435952+paulteoc@users.noreply.github.com>
2019-12-06 12:30:26 +0200

    Add files via upload

---
+++ Dispozitive Mobile/svg/svg.js
@@ -0,0 +1,43 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.12.06.0";
+
+window.addEventListener("deviceorientation", on_orientation_uab);
+window.addEventListener("devicemotion", on_motion_uab);
+
+function desenare(beta, gamma)
+{
+	var svg = document.getElementById("id_svg");
+	var cerc = document.getElementById("id_circle");
+	
+    var r = cerc.getAttribute("r");
+    var svg_height = svg.getAttribute("height");
+    var svg_width = svg.getAttribute("width");
+	
+	var x = svg_width / 2 + beta / 90 * (svg_width / 2 - r);
+    var y = svg_height / 2 + gamma / 90 * (svg_height / 2 - r);
+    
+    cerc.setAttribute("cx", x);
+    cerc.setAttribute("cy", y );
+}
+
+function on_orientation_uab(e)
+{
+
+	
+	desenare(e.beta, e.gamma);
+}
+
+function on_motion_uab(e)
+{
+	var acc = e.accelerationIncludingGravity;
+	
+	
+	
+	var beta = -Math.atan(acc.y / acc.z) / Math.PI * 180;
+	var gamma = Math.atan(acc.x / acc.z) / Math.PI * 180;
+	
+	document.getElementById("id_beta_acc").innerHTML = Math.round(beta * 100) / 100;
+	
+	document.getElementById("id_gamma_acc").innerHTML = Math.round(gamma * 100) / 100;
+	
+	desenare(beta, gamma);
+}
